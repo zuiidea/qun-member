ea01224fc84c94578738c50bbab161d1fdb50e6b
zuiidea <zuiiidea@gmail.com>
2016-07-17 00:26:53 +0800

    init

--- models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-var userScheMa = new Schema({
-	id: String,
-	qq: String,
-  name:String,
-  avatar:String
-});
-exports.user = mongoose.model('users', userScheMa);
